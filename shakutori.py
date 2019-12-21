46c8375efbaa0354fd1f71d4d8ebed3b2bf58174
gohst9 <blackgohstno9@yahoo.co.jp>
2019-12-21 21:10:21 +0900

    うまく尺取れるようになった

--- shakutori.py
+++ shakutori.py
@@ -8,13 +8,13 @@ class Shakutori:
     def calc(self):
         l = 0
         r = 1
-        while l <= len(self.lst) and r < len(self.lst):
+        while l <= len(self.lst) and r < len(self.lst)+1:
             if self.judge(self.lst[l:r]):
                 self.answer_update()
                 print(self.lst[l:r])
 
-            #ここの尺取らせ方が間違ってるからちゃんと考えようね！
-            if (self.judge(self.lst[l:r]) and r < len(self.lst)) or l >= r-1:
+            #rを+1したとき条件に合うか判定。合えばr+=1 合わない、合わないならl+=1
+            if  (r <= len(self.lst)+1 and self.judge(self.lst[l:r+1])) or l >= r-1:
                 r += 1
             else:
                 l += 1
@@ -35,7 +35,7 @@ class Shakutori:
         self.answer += 1
 
 
-lst = [1,2,3,4,5,6,7,8,9]
+lst = [1,2,3,4,5,6,7,2,3]
 s = Shakutori(lst,0,5)
 print(s.calc())
 
