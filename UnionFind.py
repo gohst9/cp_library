e09b0d33d0848ae5f1819f947cef87e2ab927dc5
gohst9 <blackgohstno9@yahoo.co.jp>
2020-03-09 21:26:53 +0900

    ABC157で新しく作ったUnionFind　古いのはポイーで

--- UnionFind.py
+++ UnionFind.py
@@ -1,69 +1,40 @@
-#-------------------------------------------------------------------------------
-# Name:        module1
-# Purpose:
-#
-# Author:      user
-#
-# Created:     08/01/2020
-# Copyright:   (c) user 2020
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+#import sys
+#sys.setrecursionlimit(500000)　マージテクをちゃんとしていればこんなものはいらないはず……
 
-class UnionFind:
-    def __init__(self,value):
-        self.value = value
-        self.parent  = self
-        self.rank = 0
-    def union(self,UF):
-        #UF別のUnion-Find木
-        if self.rank >= UF.rank:
-            self.parent = UF
-            UF.rank += 1
+class UF:
+    #経路圧縮＋マージテクのunion find木
+    def __init__(self,n):
+        self.n = n
+        self.parents = [-1] * (n+1) #1-indexedにするために１個足している
+        self.rnks = [0] * (n+1)
+
+    def find(self,n):
+        if self.parents[n] < 0:
+            return n
         else:
-            UF.parent = self
-            self.rank += 1
-
-    def find(self,lst=[]):
-        #lstには探索中根ではなかった要素を入れていき、根が見つけたときに
-        #まとめてparentを根要素に付け替える。（経路圧縮）
-        if self.parent == self:
-            for i in lst:
-                i.parent = self
-            return self
-        else:
-            lst.append(self)
-            return self.parent.find(lst)
-    def isSame(self,UF):
-
-        if self.find() == UF.find():
-            return True
-        else:
-            return False
-
-def main():
-    lst = ["taro","jiro","saburo","shiro"]
-    lst = list(map(UnionFind,lst))
-    befriends = lambda a,b:lst[a].union(lst[b]) #befriends(a,b) aがbの友達になる。
-    befriends(0,1)
-    befriends(1,2)
-
-    print(lst[1].find())
-    print(lst[2].find())
-    print(lst[1].isSame(lst[2]))
-    if lst[1].isSame(lst[2]):
-        print(lst[1].value + "くんは" + lst[2].value + "くんと友達です" )
-
-    for i in range(len(lst)):
-        for j in range(i+1,len(lst)):
-            if lst[i].isSame(lst[j]):
-                print(str(i) + lst[i].value + "くんは" + str(j) + lst[j].value + "くんと友達です" )
-            else:
-                print(str(i) + lst[i].value + "くんは" + str(j) + lst[j].value + "くんと友達じゃないです" )
+            return self.find(self.parents[n])
 
 
+    def union(self,a,b):
+        a_par = self.find(a)
+        b_par = self.find(b)
 
+        if a_par == b_par:
+            return
+        ##union by rank
+        #if self.rnks[a_par] < self.rnks[b_par]: #ランクの高い方の根に低い方の根をつける　逆にしないように……！
+        #    a_par,b_par = b_par,a_par
+        #if self.rnks[a_par] == self.rnks[b_par]:
+        #    self.rnks[a_par] += 1
+        if self.size(a_par) < self.size(b_par): #parentsの値が負の数のとき、それはその木のサイズを表す。
+            a_par,b_par = b_par,a_par
+        self.parents[a_par] += self.parents[b_par]
+        self.parents[b_par] = a_par
 
+    def size(self,n):
+        par = self.find(n)
+        return abs(self.parents[par])
 
+    def is_same(self,a,b):
+        return self.find(a) == self.find(b)
 
-if __name__ == '__main__':
-    main()
