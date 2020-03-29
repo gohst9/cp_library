6d92b3a471db7cd4fd4b3d40a91049de7e85852c
gohst9 <blackgohstno9@yahoo.co.jp>
2020-03-29 22:06:05 +0900

    スタートからつながる全ノードへの最短距離リストを作る幅優先探索

---
+++ bfs.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(graph,v=0):
+    #args:
+    #   graph グラフ本体
+    #   start スタート地点
+    dist = [-1]*len(graph)
+    q = deque()
+    q.append(v)
+    dist[v] = 0
+    while q:
+        d = dist[v]
+        v = q.popleft()
+        for v in graph[v]:
+            if dist[v] != -1:
+                continue
+            else:
+                q.append(v)
+                dist[v] = d+1
+    return dist
+
+
+def main():
+    print(bfs([[4],[],[],[],[]],0))
+    print(bfs([[1,2,3],[],[],[4],[0]]))
+
+if __name__ == '__main__':
+    main()
