b348cb3cafff6b494ce18b0e6f3fe7d688b44163
gohst9 <blackgohstno9@yahoo.co.jp>
2021-02-12 11:52:52 +0900

    優先度付きキューを用いたダイクストラ法

--- dijkstra.py
+++ dijkstra.py
@@ -1,44 +1,50 @@
+#ダイクストラ法
+
+import heapq #優先度付きキュー
+
+
 def dijkstra(graph,s=0,inf = 999999999999):
-    #graphは隣接行列
-    #sはスタート地点
-    n = len(graph) #グラフの長さ
-    costs = [inf for _ in range(n)] #移動にかかるコスト
-    fixed = [False for _ in range(n)] #確定フラグ
-    costs[s] = 0 #スタート地点はコストゼロ
-    while True:
-        mn_index = -1
-        mn = inf
-        #現在もっとも移動コスト小さいがノードを見つける
-        for i,cost in enumerate(costs):
-            if fixed[i]:
-                continue
-            if cost < mn:
-                mn = cost
-                mn_index = i
-        #未確定ノードが無かった場合ループから抜ける
-        if mn_index == -1:
+    # graph: グラフ本体。連結リスト。(次のノード,コスト)のタプルで表現
+    # s: スタートノード
+    num_of_nodes = len(graph)
+    costs = [inf for _ in range(num_of_nodes)]
+    before = [i for i in range(num_of_nodes)]
+    fixed = [False for _ in range(num_of_nodes)] # 最小コストが確定したかのフラグ
+    q = [] # 現在の最小コストを求めるためのヒープキュー
+    num_of_not_fixed_node = num_of_nodes
+
+    costs[s] = 0 # スタートノードのコストは0
+    fixed[s] = True
+    num_of_not_fixed_node -= 1
+    heapq.heappush(q,(costs[s],s)) #コストが低いものを
+
+    # 全てのノードの最小コストが確定するまでループ
+    while num_of_not_fixed_node > 0:
+        if not q:
             break
-        fixed[mn_index] = True
-        for nxt,cost in enumerate(graph[mn_index]):
-            costs[nxt] = min(costs[nxt],costs[mn_index] + cost)
+        _,shortest_node = heapq.heappop(q)
+        fixed[shortest_node] = True
+        for nxt,nxt_cost in graph[shortest_node]:
+            if not fixed[nxt] and before[shortest_node] != nxt:
+                if costs[nxt] > costs[shortest_node] + nxt_cost:
+                    costs[nxt] = costs[shortest_node] + nxt_cost
+                    before[nxt] = shortest_node
+                heapq.heappush(q,(costs[nxt],nxt))
     return costs
+
 
 
 
 def main():
     inf = 999999999999
-    n,m = map(int,input().split()) #n個のノードにm個の辺
-    s,g = map(int,input().split())
-    graph = [[inf for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        graph[i][i] = 0 #自分自身への移動はコスト0
+    n,m,s = map(int,input().split())
+    graph = [[] for _ in range(n)]
     for _ in range(m):
         a,b,c = map(int,input().split())
-        graph[a][b] = c
-    costs = dijkstra(graph,s,inf)
-    print(costs[g])
-
-
+        graph[a].append((b,c))
+    costs = dijkstra(graph,s)
+    for i in range(n):
+        print(costs[i] if costs[i] != inf else "INF")
 
 if __name__ == '__main__':
     main()
