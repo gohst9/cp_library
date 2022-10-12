752d05f197d379b2267ae5e1d18002851cf600fe
black9 <blackgohstno9@yahoo.co.jp>
2022-10-12 11:34:36 +0900

    変数名修正

--- longest_streak.py
+++ longest_streak.py
@@ -1,17 +1,16 @@
 def func(s,c1="1",c2="0"):
     """'1'と'0'で構成される文字列の中の
         最も'1'が長く続く部分の長さを求める"""
-    i,j = 0,0
-    end = len(s)
+    left,right = 0,0
     mx = -1
     for c in s:
         if c == "1":
-            j += 1
+            right += 1
         else:
-            mx = max(mx,j-i)
-            j += 1
-            i = j
-    mx = max(mx,j-i)
+            mx = max(mx,right-left)
+            right += 1
+            left = right
+    mx = max(mx,right-left)
     return mx
 
 def main():
