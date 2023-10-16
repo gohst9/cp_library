ff3b55f6ccb8d9fd5a77b35feee13d1dacd22160
black9 <blackgohstno9@yahoo.co.jp>
2023-10-16 23:14:00 +0900

    連続判定の関数を自由に選べるように

--- sequence_check.py
+++ sequence_check.py
@@ -1,57 +1,51 @@
 
-def wf(graph,inf = float("inf")):
-    total = 0 #ABC208 D問題　Shortest Path Queries 2用
-    #graph[i][j] = iからjへ行くのにかかるコスト
-    w = len(graph[0])
-    h = len(graph)
-    assert w == h,"グラフの形が不適切"
-    for k in range(w):
-        for i in range(w):
-            for j in range(w):
-                graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
-                if graph[i][j] < inf:
-                    total += graph[i][j]
-    return total
 
-class Ad_lst:
-    def __init__(self,n,directional = True,inf = float("inf")):
-        self.n = n
-        self.lst = [[inf for _ in range(n)] for _ in range(n)]
-        self.directional = directional
-        self.graph_init()
-
-    def add(self,a,b,cost,input_zero_indexed = True):
-        if not input_zero_indexed:
-            a -= 1
-            b -= 1
-        self.lst[a][b] = cost
-        if not self.directional:
-            self.lst[b][a] = cost
-
-    def get(self):
-        return self.lst
-
-    def graph_init(self):
-        #隣接リストで、自分自身への移動コストを0にする
-        n = len(self.lst)
-        for i in range(n):
-            self.lst[i][i] = 0
+def sequence_check(lst,start=0,check=lambda a,b:a==b,only_length=False):
+    #check: 　「連続しているか」を判定する関数。デフォルトだと同じ値
+    #only_lengths:　シークエンスの長さだけを返す
+    temp = lst[start]
+    length = 1
+    answer = []
+    for i in range(start,len(lst)):
+        if i+1 >= len(lst):
+            answer.append((temp,length))
+            break
+        if not check(temp,lst[i+1]):
+            answer.append((temp,length))
+            temp = lst[i+1]
+            length = 1
+            continue
+        temp = lst[i+1]
+        length += 1
+    if only_length:
+        return [ans[1] for ans in answer]
+    return answer
 
 
+import unittest
+class Test(unittest.TestCase):
+    def test_1(self):
+        lst = [1,1,1,2,2,2]
+        expected = sequence_check(lst)
+        self.assertEqual(expected,[(1,3),(2,3)])
 
+    def test_2(self):
+        lst = [1,2,3,2,3]
+        expected = sequence_check(lst,check=lambda a,b:a <= b,only_length=True)
+        self.assertEqual(expected,[3,2])
+
+    def test_3(self):
+        lst = [1,2,3]
+        expected = sequence_check(lst)
+        self.assertEqual(expected,[(1,1),(2,1),(3,1)])
+
+    def test_4(self):
+        lst = [0]
+        expected = sequence_check(lst)
+        self.assertEqual(expected,[(0,1)])
 
 def main():
-    n,m = map(int,input().split())
-    graph = Ad_lst(n)
-    for _ in range(m):
-        a,b,c = map(int,input().split())
-        graph.add(a,b,c,input_zero_indexed = False)
-    graph = graph.get()
-    print(wf(graph))
-
-
-
-
+    unittest.main()
 
 if __name__ == '__main__':
     main()
