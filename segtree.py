314b4c24f62e49d3fac4500114ace44a3f8c6869
black9 <blackgohstno9@yahoo.co.jp>
2024-02-04 13:15:22 +0900

    再帰関数をやめて高速化

--- segtree.py
+++ segtree.py
@@ -1,10 +1,11 @@
 class SegTree:
 
-    def __init__(self,lst=[],func=lambda a,b:a+b,e=0):
+    def __init__(self,n=-1,lst=[],func=lambda a,b:a+b,e=0):
         self.func = func
         self.e = e
-        n = len(lst)
         x = 1
+        if n == -1:
+            n = len(lst)
         while x < n:
             x *= 2
         self.n = x
@@ -13,18 +14,27 @@ class SegTree:
             self.update(i,v)
 
 
-
-
     def update(self,i,x):
-        i += self.n-1
+        i += self.n - 1
         self.seg[i] = x
         while i > 0:
             i = (i-1)//2
             self.seg[i] = self.func(self.seg[i*2+1],self.seg[i*2+2])
 
-    def query(self,a,b):
-        assert a <= b
-        return self.sub_query(a,b,0,0,self.n)
+    def query(self,l,r):
+        assert l <= r
+        l += self.n -1
+        r += self.n -1
+        ans = self.e
+        while (l<r):
+            if not l&1:
+                ans  = self.func(ans,self.seg[l])
+                l+=1
+            if not r&1:
+                ans = self.func(ans,self.seg[r-1])
+            l = (l-1)//2
+            r = (r-1)//2
+        return ans
 
     def sub_query(self,a,b,k,l,r):
         if b <= l or a >= r: #範囲外
@@ -35,15 +45,3 @@ class SegTree:
             l_ans = self.sub_query(a,b,k*2+1,l,(l+r)//2)
             r_ans = self.sub_query(a,b,k*2+2,(l+r)//2,r)
             return self.func(l_ans,r_ans)
-
-def main():
-    lst = [1,2,3,4,5,6,7,8]
-    seg = SegTree(lst,func=max)
-    ans = seg.query(0,3)
-    print(ans)
-    seg.update(5,5)
-    ans = seg.query(4,5)
-    print(ans)
-
-if __name__ == '__main__':
-    main()
