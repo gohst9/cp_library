b4d903dd9e6034018e91778fe7c7264bee8465d4
black9 <blackgohstno9@yahoo.co.jp>
2025-09-10 01:14:54 +0900

    高速化

--- longest_path.py
+++ longest_path.py
@@ -1,14 +1,19 @@
-from functools import lru_cache
+from functools import cache
+import sys
+sys.setrecursionlimit(10**6)
 
-@lru_cache
-def solve(x,graph):
-    if len(graph[x]) == 0:
-        return 0
-    mx = -1
-    for nxt in graph[x]:
-        mx = max(mx,solve(nxt,graph))
-    return mx + 1
+#tupleに関する色んなものが遅い　tupleは使わないようにしよう
+#lru_cacheはmaxsizeを指定する必要がある
+#cacheならデフォルトでmaxsize=None
 
+#@lru_cache
+#def solve(x,graph):
+#    if len(graph[x]) == 0:
+#        return 0
+#    mx = -1
+#    for nxt in graph[x]:
+#        mx = max(mx,solve(nxt,graph))
+#    return mx + 1
 def to_tuple(graph):
     #二次元リストを二次元タプルに
     return tuple([tuple(graph[x]) for x in range(len(graph))])
@@ -22,9 +27,20 @@ def main():
         y -= 1
         graph[x].append(y)
     mx = -1
-    graph = to_tuple(graph)
+    dp = [None for _ in range(n+1)]
+    @cache
+    def solve(x):
+
+        if len(graph[x]) == 0:
+
+            return 0
+        mx = -1
+        for nxt in graph[x]:
+            mx = max(mx,solve(nxt))
+        return mx + 1
+
     for i in range(n):
-        mx = max(mx,solve(i,graph))
+        mx = max(mx,solve(i))
     
     print(mx)
 
